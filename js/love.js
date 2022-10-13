3538243595a3e32d1a9cff7790ca73fc3b97a12d
DorthanHc.Pan <dorthanhc.pan@ogilvy.com>
2022-10-13 17:32:13 +0800

    fix

--- js/love.js
+++ js/love.js
@@ -11,7 +11,9 @@ function vueSet() {
         data: {
             package: 0,
             totalObj: 0,
-            totalAll: 1000,
+            totalAll: 0,
+            totalPay: 0,
+            sale: 400,
             T: 0,
             TBS: 0,
             TBM: 0,
@@ -28,15 +30,15 @@ function vueSet() {
             bigTowel: 0,
             smallTowel: 0,
             sock: 0,
-            price:{
-                T: '500',
-                bag: '500',
-                card: '300',
-                bigTowel: '500',
-                smallTowel: '400',
-                sock: '200'
+            price: {
+                T: 500,
+                bag: 500,
+                card: 300,
+                bigTowel: 500,
+                smallTowel: 400,
+                sock: 200
             },
-            priceText:{
+            priceText: {
                 T: 'NT$500',
                 bag: 'NT$500',
                 card: 'NT$300',
@@ -56,18 +58,33 @@ function vueSet() {
                 return ("NT$" + (Math.round(_totleAll * 100) / 100).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
             },
             planningTotla: function () {
-                this.T = this.TBS + this.TBM + this.TBL + this.TBXL + this.TWS + this.TWM + this.TWL + this.TWXL;
-                this.bag = this.bagB + this.bagW;
+                this.T = Number(this.TBS) + Number(this.TBM) + Number(this.TBL) + Number(this.TBXL) + Number(this.TWS) + Number(this.TWM) + Number(this.TWL) + Number(this.TWXL);
+                this.bag = Number(this.bagB) + Number(this.bagW);
 
-                this.totalObj = this.T + this.bag + this.card + this.bigTowel + this.smallTowel +
+                this.totalObj = Number(this.T) + Number(this.bag) + Number(this.card) + Number(this.bigTowel) + Number(this.smallTowel) + Number(this.sock);
 
-                    this.totalAll = 0;
-            }
+                this.totalAll = Number(this.T * this.price.T) + Number(this.bag * this.price.bag) + Number(this.card * this.price.card) + Number(this.bigTowel * this.price.bigTowel) + Number(this.smallTowel * this.price.smallTowel) + Number(this.sock * this.price.sock);
+
+                this.package = Math.min(this.T, this.bag, this.card, this.bigTowel, this.smallTowel, this.sock);
+
+                this.totalPay = this.totalAll - (this.package * this.sale);
+            },
+            submitOrder: function(){
+
+            },
         },
         computed: {
             totalAllText: function () {
                 this.planningTotla();
                 return this.ThousandsSign(this.totalAll);
+            },
+            totalPayText: function () {
+                this.planningTotla();
+                return this.ThousandsSign(this.totalPay);
+            },
+            packageSaleText: function () {
+                this.planningTotla();
+                return this.ThousandsSign(this.package * this.sale);
             },
         },
         updated: function () {
